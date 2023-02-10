08d7e31d66c78de6def5d99b6ced914aa71c690e
Xapollon430 <Xapollon430@gmail.com>
2023-02-10 16:10:37 -0500

    reset

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!");
 });
 
 app.listen(port, () => {
