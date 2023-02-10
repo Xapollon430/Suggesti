ab82df167462a2c9af2c9f863f457724dfff1a13
Xapollon430 <Xapollon430@gmail.com>
2023-02-09 20:48:26 -0500

    init boilerplate

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config(); // this loads env vars
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = 4000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
