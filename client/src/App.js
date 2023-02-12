ed09aade0fd277d17df0d1184d9e5d7cce2e8f1b
Xapollon430 <Xapollon430@gmail.com>
2023-02-11 20:48:49 -0500

    13

--- client/src/App.js
+++ client/src/App.js
@@ -153,7 +153,7 @@ export default function DrawerAppBar() {
 
   function handleChange(event) {
     setLoading(true);
-    fetch(`https://fern-boilerplate.vercel.app/insights`, {
+    fetch(`http://localhost:4000/insights`, {
       method: "POST",
       body: JSON.stringify({ cheap, eco, quality, custom }),
       headers: {
